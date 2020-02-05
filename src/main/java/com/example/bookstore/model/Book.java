f4cd165b8a4928d81c75c6add99f8ff46338b664
tuukkakotilainen <tuukka.kotilainen1@gmail.com>
2020-02-05 15:06:54 +0200

    First commit

---
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,45 @@
+package com.example.bookstore.model;
+
+public class Book {
+	private String title;
+	private String author;
+	private String year;
+	private String isbn;
+	private String price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
+
+}
---
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package com.example.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	@RequestMapping("/index")
+	public String book() {
+		return "";
+	}
+	
+
+}
+
