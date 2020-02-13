e72fa7e312f60e925526f5956c90b1fe89391507
tuukkakotilainen <tuukka.kotilainen1@gmail.com>
2020-02-13 15:05:38 +0200

    commit

---
+++ src/main/java/com/example/bookstore/model/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.bookstore.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+    
+}
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,14 +1,65 @@
 package com.example.bookstore.web;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.model.BookRepository;
+
+
+
+
+
+
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository;
+	
 	@RequestMapping("/index")
 	public String book() {
 		return "";
 	}
+	
+	@RequestMapping(value= {"/booklist"})
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+	
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    } 
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
+	
+	@RequestMapping(value = "/modify/{id}", method = RequestMethod.GET)
+	public String modifyBook(@PathVariable("id") Long bookId, Model model) {
+		//Haetaan tietokannasta sql lauseella kirja jolla on tämä id ja lisätään modeliin.
+		model.addAttribute("book", repository.findById(bookId));
+		return "modifybook";
+	}
+	
 	
 
 }
